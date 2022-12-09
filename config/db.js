2ea6f3c9368501b86baac783f110c63628e387f4
iqahmediq <belkhiriaahmed617@gmail.com>
2022-12-09 15:28:59 +0100

    auth

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGOOSE_URI,{useNewUrlParser: true, useUnifiedTopology: true},()=>{
     console.log("db connected");
 });
